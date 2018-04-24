796b0481a988673848812ce5fd3ec0666ddc8c68
madzia89 <wojcicka.ma@gmail.com>
2018-04-24 10:32:01 +0200

    hoisting, let vs. var

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
 console.log("Hello, ES6!")
+
+//let vs var
+
+for (var i = 0; i < 3; i++){
+    console.log("z i " + i)
+}
+
+    console.log("i poza scope" + i)
+
+
+//w poniższym przykładzie wyskoczy błąd, bo j nie jest dostępe poza wąsami {}
+for (let j = 0; j < 3; j++){
+    console.log(j)
+}
+
+console.log("z j " + j)
+
+//REDECLARATION
+
+var name1 = 'zoska' //possible
+var name1 = 'toska'//possible
+
+let name2 = 'kaska'
+let name2 = 'baska' // impossible to nie przjedzie bo let nie pozwala na redeklarowanie!!
+nazwa2 = 'lena' // to jest możliwe bo nie deklarujemy tylko przypisujemy
+
+
+
+//HOISTING
+
+function f (){
+    c = 2
+    var c
+    console.log("hoisting var", c)
+}
+f()
+
+function g() {
+    c = 2
+    let c
+    console.log('hoisting let ', c) // undefined bo let jest poniżej wartości c
+}
+g()
+
